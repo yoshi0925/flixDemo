e3023b9c037b7fc600d42d42db99d0883c31de79
yoshi <yoshi0925@gmail.com>
2019-02-19 23:44:57 -0800

    commit

---
+++ flixDemo/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  flixDemo
+//
+//  Created by Zi Yang on 2/19/19.
+//  Copyright © 2019 Zi Yang. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterLabel: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
