8200f8d8441c5380e088926681bbbc7e51643c6e
yoshi <yoshi0925@gmail.com>
2019-02-26 23:26:58 -0800

    Finished collection view layout setup

---
+++ flixDemo/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixDemo
+//
+//  Created by Zi Yang on 2/26/19.
+//  Copyright © 2019 Zi Yang. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
